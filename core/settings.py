2fe46a3db0cfb96ce46329fcd16c6f07efb82216
Mahmudjon2919 <jalilovshoxruh555@gmail.com>
2024-09-16 05:01:40 -0500

    yakunlandi

--- core/settings.py
+++ core/settings.py
@@ -160,3 +160,5 @@ CONTRACT_URL="contracts"
 CONTRACT_ROOT=BASE_DIR /"contracts"
 
 BOT_TOKEN = config("BOT_TOKEN")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000']
